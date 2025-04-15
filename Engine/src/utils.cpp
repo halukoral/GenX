ba12a75e0c0520a1741e081314adaf006511c2a5
Haluk Oral <67242728+halukoral@users.noreply.github.com>
2025-04-16 00:15:14 +0300

    Update utils.cpp

--- Engine/src/utils.cpp
+++ Engine/src/utils.cpp
@@ -1,6 +1,8 @@
 #include <pch.h>
 #include <utils.h>
 
+#include "spdlog/spdlog.h"
+
 namespace Utils
 {
 	bool streq(const gsl::czstring s1, const gsl::czstring s2)
@@ -12,18 +14,27 @@ namespace Utils
 	{
 		// Qualifiers are not needed because of ADL
 		if(!exists(shaderPath))
+		{
+			spdlog::error("shaderPath not found!");
 			return {};
+		}
 
 		if(!is_regular_file(shaderPath))
+		{
+			spdlog::error("files are not regular file!");
 			return {};
+		}
 
 		std::ifstream file(shaderPath, std::ios::binary);
 		if(!file.is_open())
+		{
+			spdlog::error("file could not be opened!");
 			return {};
+		}
 
-		const uint32_t size = file_size(shaderPath);
+		const auto size = file_size(shaderPath);
 		std::vector<uint8_t> buffer(size);
-		file.read(reinterpret_cast<char*>(buffer.data()), size);
+		file.read(reinterpret_cast<char*>(buffer.data()), (long long)size);
 		return buffer;
 	}
 }
