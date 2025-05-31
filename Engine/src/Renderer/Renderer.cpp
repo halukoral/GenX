8252355c756e203f22d14a484671d755d18e03e4
Haluk Oral <67242728+halukoral@users.noreply.github.com>
2025-05-31 20:57:05 +0300

    Update Renderer.cpp

--- Engine/src/Renderer/Renderer.cpp
+++ Engine/src/Renderer/Renderer.cpp
@@ -38,6 +38,7 @@ void Renderer::InitVulkan()
     
 	// Create some models
 	auto viking = m_ModelLayer->CreateModel("../viking_room.obj", glm::vec3(0, 0, 0));
+	LoadTexture("../viking_room.png");
 }
 
 void Renderer::LoadModel(const std::string& path)
